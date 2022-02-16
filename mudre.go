ea88f83655d6d286e4f2a051742db0a3e38feae0
Retrace <99481493+RETr4ce@users.noreply.github.com>
2022-02-16 13:03:41 -0800

    clean up

--- mudre.go
+++ mudre.go
@@ -1,15 +1,7 @@
 package main
 
 import (
-	"log"
-
 	"github.com/RETr4ce/mudre/cmd"
-)
-
-var (
-	WarningLogger *log.Logger
-	InfoLogger    *log.Logger
-	ErrorLogger   *log.Logger
 )
 
 func main() {
