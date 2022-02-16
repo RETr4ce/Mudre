5a943147a933e5c026616c156e29a112a67b9af8
Retrace <99481493+RETr4ce@users.noreply.github.com>
2022-02-16 05:48:28 -0800

    Update http.go

--- tools/http.go
+++ tools/http.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"crypto/tls"
 
 	"github.com/spf13/viper"
 )
@@ -39,9 +40,10 @@ func PostToDiscord(jsonData []byte) {
 
 func GetDataFromUrl(baseURL string) ([]byte, error) {
 
+	//During testing the certificate broke of CTFTime. This will ignore the verify the legitimacy of the server
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true},}
 	//init http client, build up the request headers and send request
-
-	client := &http.Client{}
+	client := &http.Client{Transport: tr}
 	req, _ := http.NewRequest("GET", baseURL, nil)
 
 	//Prepare headers. Some websites want a user-agent else it returns an error.
